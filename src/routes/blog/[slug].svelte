4281bc9a563f1a8b83686b1580d5382cf36ca2d0
spences10 <spences10apps@gmail.com>
2021-05-04 19:15:40 +0100

    update heading

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,16 +1,16 @@
 <script context="module">
-	import { GraphQLClient } from 'graphql-request';
+  import { GraphQLClient } from 'graphql-request'
 
-	export async function load(ctx) {
-		const graphcms = new GraphQLClient(
-			'https://api-eu-central-1.graphcms.com/v2/cko631eevhvbw01xtdirc6jxa/master',
-			{
-				headers: {}
-			}
-		);
+  export async function load(ctx) {
+    const graphcms = new GraphQLClient(
+      'https://api-eu-central-1.graphcms.com/v2/cko631eevhvbw01xtdirc6jxa/master',
+      {
+        headers: {},
+      }
+    )
 
-		const { post } = await graphcms.request(
-			`query BlogPageQuery($slug :String!) {
+    const { post } = await graphcms.request(
+      `query BlogPageQuery($slug :String!) {
 				post(where: { slug: $slug }) {
 					title
 					content{
@@ -18,21 +18,21 @@
 					}
 				}
 			}`,
-			{
-				slug: ctx.page.params.slug
-			}
-		);
+      {
+        slug: ctx.page.params.slug,
+      }
+    )
 
-		return {
-			props: {
-				post
-			}
-		};
-	}
+    return {
+      props: {
+        post,
+      },
+    }
+  }
 </script>
 
 <script>
-	export let post;
+  export let post
 </script>
 
 <h1>{post.title}</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,42 @@
 <script context="module">
-	import { GraphQLClient } from 'graphql-request';
+  import { GraphQLClient } from 'graphql-request'
 
-	export async function load() {
-		const graphcms = new GraphQLClient(
-			'https://api-eu-central-1.graphcms.com/v2/cko631eevhvbw01xtdirc6jxa/master',
-			{
-				headers: {}
-			}
-		);
+  export async function load() {
+    const graphcms = new GraphQLClient(
+      'https://api-eu-central-1.graphcms.com/v2/cko631eevhvbw01xtdirc6jxa/master',
+      {
+        headers: {},
+      }
+    )
 
-		const { posts } = await graphcms.request(
-			`{
+    const { posts } = await graphcms.request(
+      `{
           posts {
             slug
             title
           }
       }`
-		);
+    )
 
-		return {
-			props: {
-				posts
-			}
-		};
-	}
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
 </script>
 
 <script>
-	export let posts;
+  export let posts
 </script>
 
-<h1>Hello</h1>
+<h1>GraphCMS blog-starter</h1>
 <ul>
-	{#each posts as post}
-		<li>
-			<a href="/blog/{post.slug}">
-				{post.title}
-			</a>
-		</li>
-	{/each}
+  {#each posts as post}
+    <li>
+      <a href="/blog/{post.slug}">
+        {post.title}
+      </a>
+    </li>
+  {/each}
 </ul>
